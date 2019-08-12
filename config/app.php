d1a0b751b40ee1de17875657de3e802926a0eb43
uki7991 <tilek.kubanov@gmail.com>
2019-08-12 19:41:37 +0600

    1c integration

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Mavsan\LaProtocol\Providers\ProtocolProvider::class,
+        Chumper\Zipper\ZipperServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Zipper' => Chumper\Zipper\Zipper::class,
 
     ],
 
