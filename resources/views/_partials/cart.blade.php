785596499871071178a9abc118b019402e4acf48
uki7991 <tilek.kubanov@gmail.com>
2019-09-05 15:40:58 +0600

    Update cart.blade.php

--- resources/views/_partials/cart.blade.php
+++ resources/views/_partials/cart.blade.php
@@ -2,43 +2,55 @@
     <h2 class="text-uppercase text-muted h5 py-4">Корзина</h2>
     @if(count($cartItems))
 
-        <div class="row">
-            <div class="col-4 h3">
-                Товар
-            </div>
-            <div class="col-2 h3">
-                Цена
-            </div>
-            <div class="col-3 h3">
-                Кол-во
-            </div>
-            <div class="col-3 h3">
-                Цена
-            </div>
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th style="text-align: center;" scope="col">Товар</th>
+                    <th style="text-align: center;" scope="col">Цена</th>
+                    <th style="text-align: center;" scope="col">Кол-во</th>
+                    <th style="text-align: center;" scope="col">Сумма</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($cartItems as $item)
+                <tr>
+                    <th scope="row" class="">
+                        <div class="d-flex">
+                            <img src="{{ asset('storage/'.\App\Book::find($item->id)->image) }}" style="height: 100px; width: auto;" alt="">
+                            <p class="small m-0 ml-3">{{ $item->name }}</p>
+                        </div>
+                    </th>
+                    <td style="vertical-align: middle;">
+                        <p class="text-center m-0">{{ $item->price }} сом</p>
+                    </td>
+                    <td style="vertical-align: middle;">
+                        <div class="d-flex mx-auto justify-content-between align-items-center" style="width: 100px;">
+                            <span class="pointer cart-btn rounded-circle shadow p-2 remove_book d-flex justify-content-center align-items-center" data-id="{{ $item->id }}">-</span>
+                            <span class="mx-2">{{ $item->quantity }}</span>
+                            <span class="pointer cart-btn rounded-circle shadow buy_book p-2 d-flex justify-content-center align-items-center" data-id="{{ $item->id }}">+</span>
+                        </div>
+                    </td>
+                    <td style="vertical-align: middle;">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <p class="m-0 text-left">{{ $item->getPriceSum() }} сом</p>
+                            <span class="ml-5 pointer cart-btn shadow rounded-circle d-flex justify-content-center align-items-center p-2 delete_book" data-id="{{ $item->id }}">&times;</span>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+                </tbody>
+                <tfoot>
+                <tr>
+                    <th style="text-align: center;" scope="col">Товар</th>
+                    <th style="text-align: center;" scope="col">Цена</th>
+                    <th style="text-align: center;" scope="col">Кол-во</th>
+                    <th style="text-align: center;" scope="col">Сумма</th>
+                </tr>
+                </tfoot>
+            </table>
         </div>
 
-        @foreach($cartItems as $item)
-            <div class="row border-top border-bottom py-3 align-items-center">
-                <div class="col-4 d-flex align-items-center">
-                    <img src="{{ asset('storage/'.\App\Book::find($item->id)->image) }}" style="height: 100px; width: auto;" alt="">
-                    <p class="small m-0 ml-3">{{ $item->name }}</p>
-                </div>
-                <div class="col-2">
-                    <p class=" m-0">{{ $item->price }} сом</p>
-                </div>
-                <div class="col-3">
-                    <div class="d-flex justify-content-between align-items-center" style="width: 100px;">
-                        <span class="pointer cart-btn rounded-circle shadow p-2 remove_book d-flex justify-content-center align-items-center" data-id="{{ $item->id }}">-</span>
-                        <span class="mx-2">{{ $item->quantity }}</span>
-                        <span class="pointer cart-btn rounded-circle shadow buy_book p-2 d-flex justify-content-center align-items-center" data-id="{{ $item->id }}">+</span>
-                    </div>
-                </div>
-                <div class="col-3 d-flex align-items-center">
-                    <p class="m-0 text-left">{{ $item->getPriceSum() }} сом</p>
-                    <span class="ml-5 pointer cart-btn shadow rounded-circle d-flex justify-content-center align-items-center p-2 delete_book" data-id="{{ $item->id }}">&times;</span>
-                </div>
-            </div>
-        @endforeach
 
         <div class="row mt-3">
             <div class="col-9 h3">
@@ -48,6 +60,9 @@
                 {{ $total }} сом
             </div>
         </div>
+
+
+
     @else
         <div class="row justify-content-center">
             <p class="h3 text-muted">Корзина пуста!</p>
